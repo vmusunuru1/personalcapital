caf97b400f26f8ca290e1d5fa685215e224682f0
Vamsi Musunuru <vamsi.musunuru@autodesk.com>
2019-04-21 09:13:02 -0700

    Update CurlExecution.sh

--- src/Utilites/CurlExecution.sh
+++ src/Utilites/CurlExecution.sh
@@ -1,7 +1,7 @@
 #i!/bin/sh
 
 for i in {1..229}; do
-  curl -s -H "Content-Type: application/x-ndjson"  vpc-searchelastic-orarpi2udhmbvoqwta5hrn743q.us-west-2.es.amazonaws.com/_bulk --data-binary @output${i}.json -H 'Content-Type: application/json'
+  curl -s -H "Content-Type: application/json"  vpc-searchelastic-orarpi2udhmbvoqwta5hrn743q.us-west-2.es.amazonaws.com/_bulk --data-binary @output${i}.json -H 'Content-Type: application/json'
 
    echo $i
 done
